86f2a2d08d6cd06857cb581b4a8b4103a7349054
Ivan Korolkov <nerounxqxq@gmail.com>
2025-07-14 21:14:49 +0400

    Исправлен serializer

--- telemetry/collector/src/main/java/ru/yandex/practicum/controller/EventController.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/controller/EventController.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.controller;
 
 import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
--- telemetry/collector/src/main/java/ru/yandex/practicum/kafka/configuration/KafkaClientConfiguration.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/kafka/configuration/KafkaClientConfiguration.java
@@ -1,18 +1,22 @@
 package ru.yandex.practicum.kafka.configuration;
 
 import jakarta.annotation.PreDestroy;
+import lombok.Getter;
+import lombok.Setter;
 import org.apache.avro.specific.SpecificRecordBase;
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.VoidSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ru.yandex.practicum.kafka.GeneralAvroSerializer;
 
 import java.util.Properties;
 
+@Getter
+@Setter
 @Configuration
 public class KafkaClientConfiguration {
 
@@ -51,7 +55,7 @@ public class KafkaClientConfiguration {
             private void createProducer() {
                 Properties properties = new Properties();
                 properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-                properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, VoidSerializer.class);
+                properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
                 properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, GeneralAvroSerializer.class);
                 producer = new KafkaProducer<>(properties);
             }
