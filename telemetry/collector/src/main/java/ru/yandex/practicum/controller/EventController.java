797963cfbf4742fa40f220af36e2df423aaa6368
Ivan Korolkov <nerounxqxq@gmail.com>
2025-07-17 23:48:39 +0400

    Добавлен обработчик Observer + подключение к grpc в properties

--- telemetry/collector/src/main/java/ru/yandex/practicum/controller/EventController.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/controller/EventController.java
@@ -1,10 +1,11 @@
 package ru.yandex.practicum.controller;
 
 import com.google.protobuf.Empty;
+import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
 import io.grpc.stub.StreamObserver;
 import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.server.service.GrpcService;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.grpc.telemetry.collector.CollectorControllerGrpc;
 import ru.yandex.practicum.grpc.telemetry.event.HubEventProto;
@@ -17,9 +18,7 @@ import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-@RestController
 @Slf4j
-@RequestMapping("/events")
 @GrpcService
 public class EventController extends CollectorControllerGrpc.CollectorControllerImplBase {
 
@@ -33,29 +32,47 @@ public class EventController extends CollectorControllerGrpc.CollectorController
                 .collect(Collectors.toMap(SensorEventHandler::getMessageType, Function.identity()));
     }
 
-    @PostMapping("/sensors")
-    @ResponseStatus(HttpStatus.OK)
-    public void sensorEvent(SensorEventProto sensorEvent, StreamObserver<Empty> observer) {
+    @Override
+    public void collectSensorEvent(SensorEventProto sensorEvent, StreamObserver<Empty> observer) {
         log.info("Получен POST запрос /events/sensors типа {} с телом: {}", sensorEvent.getPayloadCase(), sensorEvent);
-        if (sensorEventHandlersMap.containsKey(sensorEvent.getPayloadCase())) {
-            sensorEventHandlersMap.get(sensorEvent.getPayloadCase()).handle(sensorEvent);
-            log.info("POST запрос /events/sensors успешно обработан!");
-        } else {
-            log.error("Ошибка! Неизвестный тип события для сенсора: {}", sensorEvent.getPayloadCase());
-            throw new IllegalArgumentException("Неизвестный тип события");
+        try {
+            if (sensorEventHandlersMap.containsKey(sensorEvent.getPayloadCase())) {
+                sensorEventHandlersMap.get(sensorEvent.getPayloadCase()).handle(sensorEvent);
+                log.info("POST запрос /events/sensors успешно обработан!");
+            } else {
+                log.error("Ошибка! Неизвестный тип события для сенсора: {}", sensorEvent.getPayloadCase());
+                throw new IllegalArgumentException("Неизвестный тип события");
+            }
+            observer.onNext(Empty.getDefaultInstance());
+            observer.onCompleted();
+        } catch (Exception e) {
+            observer.onError(new StatusRuntimeException(
+                    Status.INTERNAL
+                            .withDescription(e.getLocalizedMessage())
+                            .withCause(e)
+            ));
         }
     }
 
-    @PostMapping("/hubs")
-    @ResponseStatus(HttpStatus.OK)
-    public void hubEvent(HubEventProto hubEvent, StreamObserver<Empty> observer) {
+    @Override
+    public void collectHubEvent(HubEventProto hubEvent, StreamObserver<Empty> observer) {
         log.info("Получен POST запрос /events/hubs типа {} с телом: {}", hubEvent.getPayloadCase(), hubEvent);
-        if (hubEventHandlersMap.containsKey(hubEvent.getPayloadCase())) {
-            hubEventHandlersMap.get(hubEvent.getPayloadCase()).handle(hubEvent);
-            log.info("POST запрос /events/hubs успешно обработан!");
-        } else {
-            log.error("Ошибка! Неизвестный тип события для хаба: {}", hubEvent.getPayloadCase());
-            throw new IllegalArgumentException("Неизвестный тип события");
+        try {
+            if (hubEventHandlersMap.containsKey(hubEvent.getPayloadCase())) {
+                hubEventHandlersMap.get(hubEvent.getPayloadCase()).handle(hubEvent);
+                log.info("POST запрос /events/hubs успешно обработан!");
+            } else {
+                log.error("Ошибка! Неизвестный тип события для хаба: {}", hubEvent.getPayloadCase());
+                throw new IllegalArgumentException("Неизвестный тип события");
+            }
+            observer.onNext(Empty.getDefaultInstance());
+            observer.onCompleted();
+        } catch (Exception e) {
+            observer.onError(new StatusRuntimeException(
+                    Status.INTERNAL
+                            .withDescription(e.getLocalizedMessage())
+                            .withCause(e)
+            ));
         }
     }
 }
