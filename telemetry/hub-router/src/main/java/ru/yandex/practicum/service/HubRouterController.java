34f8e1eaa628c03ade1e155344ac703b19d330eb
Ivan Korolkov <nerounxqxq@gmail.com>
2025-07-21 21:37:57 +0400

    fix final final

--- telemetry/hub-router/src/main/java/ru/yandex/practicum/service/HubRouterController.java
+++ telemetry/hub-router/src/main/java/ru/yandex/practicum/service/HubRouterController.java
@@ -4,24 +4,26 @@ import com.google.protobuf.Empty;
 import io.grpc.Status;
 import io.grpc.StatusRuntimeException;
 import io.grpc.stub.StreamObserver;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.server.service.GrpcService;
 import ru.yandex.practicum.grpc.telemetry.event.DeviceActionRequest;
 import ru.yandex.practicum.grpc.telemetry.hubrouter.HubRouterControllerGrpc;
 
 @Slf4j
+@RequiredArgsConstructor
 @GrpcService
 public class HubRouterController extends HubRouterControllerGrpc.HubRouterControllerImplBase {
-    @Override
-    public void handleDeviceAction(DeviceActionRequest request, StreamObserver<Empty> observer) {
-        log.info("Получен запрос GRPC для DeviceAction: {}", request);
+    public void handleDeviceAction(DeviceActionRequest deviceActionRequest, StreamObserver<Empty> responseObserver) {
+
         try {
-            observer.onNext(Empty.newBuilder().build());
-            observer.onCompleted();
-            log.info("Запрос GRPC DeviceAction успешно обработан");
+            log.info("timestamp={}, fields={}",
+                    deviceActionRequest.getTimestamp(),
+                    deviceActionRequest.getAllFields().toString());
+            responseObserver.onNext(Empty.newBuilder().build());
+            responseObserver.onCompleted();
         } catch (Exception e) {
-            log.info("Ошибка при обработке запроса GRPC DeviceAction");
-            observer.onError(new StatusRuntimeException(
+            responseObserver.onError(new StatusRuntimeException(
                     Status.INTERNAL
                             .withDescription(e.getLocalizedMessage())
                             .withCause(e)
