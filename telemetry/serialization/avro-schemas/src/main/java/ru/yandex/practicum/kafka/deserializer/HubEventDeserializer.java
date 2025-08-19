ea95e0c4f7a3525d8556c8f1cf91cffcbb8d980c
Ivan Korolkov <nerounxqxq@gmail.com>
2025-08-20 01:29:57 +0400

    Добавлена внешняя конфигурация. Spring Cloud Config

--- telemetry/serialization/avro-schemas/src/main/java/ru/yandex/practicum/kafka/deserializer/HubEventDeserializer.java
+++ telemetry/serialization/avro-schemas/src/main/java/ru/yandex/practicum/kafka/deserializer/HubEventDeserializer.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.kafka.deserializer;
 
+
 import ru.yandex.practicum.kafka.telemetry.event.HubEventAvro;
 
 public class HubEventDeserializer extends BaseAvroDeserializer<HubEventAvro> {
