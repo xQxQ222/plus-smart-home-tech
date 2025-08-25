cbb9ff329f9a5e13883a2529b7766191221f61b1
Ivan Korolkov <nerounxqxq@gmail.com>
2025-08-25 04:14:00 +0400

    fix configuration

--- commerce/shopping-store/src/main/java/ru/yandex/practicum/ShoppingStoreApp.java
+++ commerce/shopping-store/src/main/java/ru/yandex/practicum/ShoppingStoreApp.java
@@ -2,14 +2,12 @@ package ru.yandex.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import ru.yandex.practicum.feign.client.store.StoreFeignClient;
 
 @SpringBootApplication
 @EnableConfigurationProperties
-@ConfigurationPropertiesScan
 @EnableFeignClients(clients = StoreFeignClient.class)
 public class ShoppingStoreApp {
     public static void main(String[] args) {
